6d02e86799465c7c8fde2ad7e48422a1fec18779
ahmed mazhar <ahmedmazhar666@gmail.com>
2025-04-05 20:10:01 +0200

    modified:   cmd/root.go 	modified:   go.mod 	new file:   go.sum

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,28 @@
-package cmd
+package main
 
 import (
 	"fmt"
-	// "github.com/spf13/cobra"
+
+	"github.com/spf13/cobra"
 )
 
 // Main CLI entry (cobra-based if needed)
 
-func cobr() {
+func main() {
 	fmt.Println("Hello, world!")
-	// rootCmd := &cobra.Command{
-	// 	Use:   "my-app",
-	// 	Short: "A simple CLI application",
-	// }
-	// rootCmd.AddCommand(versionCmd)
-	// rootCmd.Execute()
+	rootCmd := &cobra.Command{
+		Use:   "my-app",
+		Short: "A simple CLI application",
+	}
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "Display version information",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Version: 1.0.0")
+		},
+	}
+	rootCmd.AddCommand(versionCmd)
+
+	rootCmd.Execute()
 
 }
